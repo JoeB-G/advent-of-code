52c39b59bcb310cc3cbee1e624d87dcf1ead3601
Joseph Bodden-Glennon <joseph.bodden-glennon@dvla.gov.uk>
2023-12-05 12:24:32 +0000

    day 2 part 2 solved

--- day_2/cube_conundrum.rb
+++ day_2/cube_conundrum.rb
@@ -5,6 +5,7 @@ MAXES = {
   'green' => 13,
   'blue' => 14
 }.freeze
+
 def cube_conundrum
   input_data = File.readlines('day_2/input.txt', chomp: true)
   transformed_data = input_data.map { |line| line.split(': ') }
@@ -19,12 +20,32 @@ def cube_conundrum
 
   @result = 0
   transformed_data.each_with_index do |item, index|
-    if item
-      @result += (index + 1)
-    end
+    @result += (index + 1) if item
   end
 
   pp @result
 end
 
-cube_conundrum
+
+def cube_conundrum_p2
+  @total = 0
+  input_data = File.readlines('day_2/input.txt', chomp: true)
+  input_data.map { |line| line.split(': ') }
+            .map do |pull|
+    count = {
+      'red' => 0,
+      'green' => 0,
+      'blue' => 0
+    }
+    pull[1].split('; ')
+           .map do |item|
+      item.split(', ')
+          .map { |draw| draw.split(' ') }
+          .map { |colour| count[colour[1]] = colour[0].to_i if colour[0].to_i > count[colour[1]]}
+    end
+    @total += count['red'] * count['green'] * count['blue']
+  end
+  pp @total
+end
+
+cube_conundrum_p2
