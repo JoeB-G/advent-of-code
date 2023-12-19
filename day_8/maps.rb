60aac17db676f41778b6bebeb8247cc86040037d
Joseph Bodden-Glennon <joseph.bodden-glennon@dvla.gov.uk>
2023-12-19 13:26:11 +0000

    day 8 part 2, wonder how long that would take to brute force instead of lcm

--- day_8/maps.rb
+++ day_8/maps.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry'
 def maps
   lines = File.readlines('day_8/input.txt', chomp: true)
   directions = lines[0]
@@ -21,4 +22,27 @@ def maps
   pp steps
 end
 
-maps
+def maps_part2
+  lines = File.readlines('day_8/input.txt', chomp: true)
+  directions = lines[0]
+  network = {}
+  regex = /(?<keys>[A-Z]{3}) = \((?<lefts>[A-Z]{3}), (?<rights>[A-Z]{3})\)/
+  lines.drop(2).each do |line|
+    match_data = line.match(regex)
+    network[match_data[:keys]] = [match_data[:lefts], match_data[:rights]]
+  end
+
+  positions = network.select { |key, _value| key.match(/..A/) }.keys
+  steps = [0, 0, 0, 0, 0, 0]
+  positions.map.with_index do |position, index|
+    while position[2] != 'Z'
+      directions.each_char do |char|
+        steps[index] += 1
+        char == 'L' ? (position = network[position][0]) : (position = network[position][1])
+      end
+    end
+  end
+  pp steps.reduce(1, :lcm)
+end
+
+maps_part2
