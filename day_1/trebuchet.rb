838a12e2ea1fb467a63046955fac0c39fe329e31
Joseph Bodden-Glennon <joseph.bodden-glennon@dvla.gov.uk>
2023-12-21 12:44:12 +0000

    never finished day_1, some work on day_7

--- day_1/trebuchet.rb
+++ day_1/trebuchet.rb
@@ -1,17 +1,24 @@
-# def decode(code_string)
-#   first_digit = code_string.match(/\d/)
-#   last_digit = code_string.reverse.match(/\d/)
-#   "#{first_digit}#{last_digit}".to_i
-# end
-#
-# def decode_array
-# input_data = File.readlines('data.txt', chomp: true)
-# input_data.map { |item| decode(item) }
-#             .sum
-# end
+# frozen_string_literal: true
 
-def decode_part_2
-  input_data = File.readlines('day_1/input_data.txt', chomp: true)
+def decode(code_string)
+  first_digit = code_string.match(/\d/)
+  last_digit = code_string.reverse.match(/\d/)
+  "#{first_digit}#{last_digit}".to_i
+end
+
+def calibration_value
+  input_data = File.readlines('day_1/data.txt', chomp: true)
+  input_data.map { |item| decode(item) }
+            .sum
+end
+
+def calibration_value2
+  input_data = File.readlines('day_1/data.txt', chomp: true)
+  input_data.map { |item| decode_part2(item) }
+            .sum
+end
+
+def decode_part2(string)
   word_values = {
     'one'   => '1',
     'two'   => '2',
@@ -26,9 +33,13 @@ def decode_part_2
   re_forwards = /one|two|three|four|five|six|seven|eight|nine|\d/
   re_backwards = /eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\d/
 
-  first_digits = input_data.map { |item| item.match(re_forwards).to_s }
-  last_digits = input_data.map { |item| item.reverse.match(re_backwards).to_s.reverse }
+  first_digit = string.match(re_forwards).to_s
+  last_digit = string.reverse.match(re_backwards).to_s.reverse
 
-  pp first_digits
+  first_digit = word_values[first_digit] unless word_values[first_digit].nil?
+  last_digit = word_values[last_digit] unless word_values[last_digit].nil?
+  "#{first_digit}#{last_digit}".to_i
 end
-decode_part_2
+
+pp calibration_value
+pp calibration_value2
--- day_7/cards.rb
+++ day_7/cards.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 def cards
   lines = File.readlines('day_7/example.txt', chomp: true)
-  pp(lines.map { |line| line.split(' ') })
+  hand_bid_hash = lines.map { |line| line.split(' ') }.to_h
+  hands = hand_bid_hash.keys
+  five_of_a_kind = hands.map { |hand| hand.match?(/(.)\1{4}/) ? hand : nil }.compact
+  four_of_a_kind = hands.map { |hand| hand.match?(/(.)\1{3}/) || hand.match?(/(.).\1{3}/) ? hand : nil }.compact - five_of_a_kind
+  full_house = hands.map { |hand|
+ hand.match?(/(.)\1(.)\2{2}/) || hand.match?(/(.)\1{2}(.)\2/) ? hand : nil }.compact - five_of_a_kind
+
+
+  three_of_a_kind = hands.map { |hand| pp hand.chars.tally.values.sort }.sort
+  pp three_of_a_kind
 end
 
 cards
